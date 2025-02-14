b6165f9f2402c325571bd3e558d36d4370446cfd
hnascx <henrique.nascimento12@hotmail.com>
2025-02-14 12:17:56 -0300

    feat/ESLint configured

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,10 +6,11 @@ app.get('/', () => {
   return 'Node.js application created'
 })
 
-// app.listen returns a promise so because of that we can use .then to run code after the server is running 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP server running on http://localhost:3333')
-})
-
+// app.listen returns a promise so because of that we can use .then to run code after the server is running
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
