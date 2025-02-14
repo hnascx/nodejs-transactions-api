209b4350687000ce82eb72cba18ee5ecc7a7f9d0
hnascx <henrique.nascimento12@hotmail.com>
2025-02-14 11:56:01 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify"
+
+const app = fastify()
+
+app.get('/', () => {
+  return 'Node.js application created'
+})
+
+// app.listen returns a promise so because of that we can use .then to run code after the server is running 
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP server running on http://localhost:3333')
+})
+
