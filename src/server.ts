1660f8a55439b0cf7026270be480dc6624945073
hnascx <henrique.nascimento12@hotmail.com>
2025-02-17 19:21:53 -0300

    feat/Testing transaction creation

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,6 @@
-import fastify from 'fastify'
-import cookie from '@fastify/cookie'
-
+import { app } from './app'
 import { env } from './env'
-import { transactionsRoutes } from './routes/transactions'
 
-const app = fastify()
-
-app.register(cookie)
-
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
-
-// app.listen returns a promise so because of that we can use .then to run code after the server is running
 app
   .listen({
     port: env.PORT,
@@ -20,3 +8,5 @@ app
   .then(() => {
     console.log('HTTP server running on http://localhost:3333')
   })
+
+// app.listen returns a promise so because of that we can use .then to run code after the server is running
---
+++ test/example.spec.ts
@@ -0,0 +1,22 @@
+import { test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('The user must be able to create a new transaction', async () => {
+  await request(app.server) // node server
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
+})
